e0709dddd710c33eb0c3a64f4cff1ee3dfec90ea
ColotVS <kolotilov98@mail.ru>
2024-03-01 15:40:01 +0700

    Работа с SimpleDateFormat Урок 17, задача 22

---
+++ lesson_17_22.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class lesson_17_22 {
+    //Исправь ошибку, чтобы программа вывела "2010-01-06".
+    //Инициализацию переменной date не меняй.
+    public static void main(String[] args) {
+        Date date = new Date("6 January 2010");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(formatter.format(date));
+    }
+}
