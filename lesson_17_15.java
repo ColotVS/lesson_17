118200c3adf660c08b5f878b551bec6b84585233
ColotVS <kolotilov98@mail.ru>
2024-03-01 15:24:34 +0700

    Работа с фрагментами даты, часть 2 Урок 17, задача 15

---
+++ lesson_17_15.java
@@ -0,0 +1,11 @@
+public class lesson_17_15 {
+    //У объекта Date отдельные фрагменты даты можно не только получать, но и изменять:
+
+    //Метод	                            Описание
+    //void setYear(int year)	        Меняет год даты. Год задаётся относительно 1900 года.
+    //void setMonth(int month)	        Меняет месяц даты, месяцы нумеруются с нуля
+    //void setDate(int date)	        Меняет день месяца
+    //void setHours(int hours)	        Меняет часы
+    //void setMinutes(int minutes)	    Меняет минуты
+    //void setSeconds(int seconds)	    Меняет секунды
+}
